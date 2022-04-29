97305082e57c6b701ae3e3ae61fb8b844efa6a4b
Oskar <oskar.rokhlin@gmail.com>
2022-04-29 16:25:33 +0100

    mapping of model works --> can calculate y array for an arbitrary model function

--- dfit.cpp
+++ dfit.cpp
@@ -9,9 +9,10 @@ using std::cout;
 using std::cin;
 
 
-long double MODEL_CexpMx(long double& x, vector<long double>& params, int num)
+long double MODEL_CexpMx(long double x, vector<long double> params, int num)
 {
 	long double result;
+	cout << x << std::endl;
 	result = params[0] * expl(params[1]/x);
 	return result;
 }
@@ -28,7 +29,7 @@ long double MODEL_POLY(long double& x, vector<long double>& params, int num)
 	return result;
 }
 
-long double MODEL_LOG(long double& x, vector<long double>& params, int num)
+long double MODEL_LOG(long double x, vector<long double> params, int num)
 {
 	long double result;
 	result = params[0] * log10(x) + params[1];
@@ -83,11 +84,14 @@ long double CalcMSR(vector<long double>& residuals, int& size)
 }
 
 // returns vector of y values predicted by model
-vector<long double> MAP_MODEL(vector<long double>& x_vals, vector<long double> params, int& set_size, int& p_size, std::function<long double> model)
+vector<long double> MAP_MODEL(vector<long double>& x_vals, vector<long double> params, int set_size, int p_size, std::function<long double (long double, vector<long double>, int)> model)
 {
 	vector<long double> f_val;
+	cout << "MAPPING \n";
+
 	for (int i=0; i < set_size; ++i)
 	{
+
 		f_val.push_back(model(x_vals[i],params,p_size));
 	}
 	return f_val;
@@ -106,7 +110,7 @@ vector<long double> EXP_MODEL_GRAD_dS_da(vector<long double>& residuals, vector<
 		grad_j = 0.0l;
 		for(int i=0; i < size; ++i)
 		{
-			grad_j += (-1.0l * MODEL_CexpMx(x_vals[i]) * residuals[i])/params[j];
+			grad_j += (-1.0l * MODEL_CexpMx(x_vals[i],params,2) * residuals[i])/params[j];
 		}
 		grad_j = 2.0l * grad_j / size;
 		grad.push_back(grad_j);
@@ -117,26 +121,26 @@ vector<long double> EXP_MODEL_GRAD_dS_da(vector<long double>& residuals, vector<
 // Returns vector containing optimised model parameters (and final MSR) as calculated by steepest descent
 // Start by implementing algorithm for just the small exponential model --> then expand for model choice
 // init_param --> guesses for parameters, but should we have the lambda here? change later perhaps
-vector<long double> minimise_msr(vector<long double>& x_vals, vector<long double>& y_data, vector<long double>& init_param, int set_size)
-{
-	// Let's assume that we're getting the number of parameters correct -> for each model it'll be 'hardcoded'
-	int num_parameters = 2; // Recall that we're just starting with the exp model
-	int max_iteration = 10000; // maximum number of iterations before giving up
-
-	long double lambda = 0.001; // greed parameter
-	long double delta_s; // change in S from step to step;
-	// Calculate the vector?
-
-	// Calculate the initial residuals, S, vector
-	vector<long double> residuals = CalcRes()
-
-	int i = 0;
-	while(i < max_iteration) // we'll bring in the other criteria
-	{
-
-		++i;
-	}
-}
+// vector<long double> minimise_msr(vector<long double>& x_vals, vector<long double>& y_data, vector<long double>& init_param, int set_size)
+// {
+// 	// Let's assume that we're getting the number of parameters correct -> for each model it'll be 'hardcoded'
+// 	int num_parameters = 2; // Recall that we're just starting with the exp model
+// 	int max_iteration = 10000; // maximum number of iterations before giving up
+//
+// 	long double lambda = 0.001; // greed parameter
+// 	long double delta_s; // change in S from step to step;
+// 	// Calculate the vector?
+//
+// 	// Calculate the initial residuals, S, vector
+// 	vector<long double> residuals = CalcRes()
+//
+// 	int i = 0;
+// 	while(i < max_iteration) // we'll bring in the other criteria
+// 	{
+//
+// 		++i;
+// 	}
+// }
 
 
 //TEST
@@ -241,6 +245,29 @@ namespace DBG
 		}
 	}
 
+	void TEST_MAP_MODEL(std::string test_name)
+	{
+		cout << "\n" << test_name << std::endl;
+		vector<long double> t_param = {1.2l,3.1l};
+		vector<long double> x_vals = {1.0l,2.0l,3.0l,4.0l};
+		// but what to do now?
+		vector<long double> TEST_EXP = MAP_MODEL(x_vals, t_param, 4, 2, MODEL_CexpMx);
+		vector<long double> TEST_LOG = MAP_MODEL(x_vals, t_param, 4, 2, MODEL_LOG);
+		vector<long double> expon_expected;
+		vector<long double> log_expected;
+
+		for(int i = 0; i < 4; ++i)
+		{
+			expon_expected.push_back(MODEL_CexpMx(x_vals[i],t_param,2));
+			log_expected.push_back(MODEL_LOG(x_vals[i],t_param,2));
+		}
+
+		cout << "TEST 1: Exponential Model" << std::endl;
+		DEBUG_PrintVec(TEST_EXP,expon_expected, 4);
+		cout << std::endl << "TEST 2: Log10 Model" << std::endl;
+		DEBUG_PrintVec(TEST_LOG,log_expected, 4);
+	}
+
 
 	void RUN_TEST_HARNESS()
 	{
@@ -251,6 +278,8 @@ namespace DBG
 		TEST_MODEL_POLY("TEST: Calculating polynomial model");
 
 		TEST_MODEL_LOG("TEST: Calculating log10 model");
+
+		TEST_MAP_MODEL("TEST: Map model function (f(x))");
 	}
 }
 
